bfc4f5be43a0050a882a21834df36d476450fa3e
gowtham-h <hosur.gowtham@gmail.com>
2016-02-01 17:19:03 +0530

    Added Modal Popup and comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-
-$(function(){
-  $('#alertMe').click(function(e){
-    e.preventDefault();
-    $('#successAlert').slideDown();
-  })
-})
+$(function() {
+$('[data-toggle="tooltip"]').tooltip(); }
+)
